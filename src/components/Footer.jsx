7b48b9275c18a48d4193f41b6a04f226ca0c9026
Tetsu <ttgchang@gmail.com>
2026-02-12 10:39:14 +0800

    feat:move logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-
+import logo from "../assets/images/Handmade_Bow.png";
 const Footer = () => {
     return (<>
     <nav className="navbar bg-body-tertiary">
         <div className="container-fluid justify-content-center">
             <Link className="navbar-brand" to="/">
-            <img src="/Handmade_Bow.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top me-2" />
+            <img src={logo} alt="Logo" width="30" height="30" className="d-inline-block align-text-top me-2" />
             @2026 愛哆啦也愛手作</Link>
         </div>
     </nav>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/images/Handmade_Bow.png";
 const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">
-                <img src="/Handmade_Bow.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top me-2" />
+                <img src={logo} alt="Logo" width="30" height="30" className="d-inline-block align-text-top me-2" />
                 愛哆啦也愛手作</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
